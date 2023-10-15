161e129c1809e65c09c594a3b7fbdc9093d923f3
Emilien PAUL <emilienpaul@hotmail.fr>
2023-10-15 13:50:17 +0200

    question

--- pages/outil.vue
+++ pages/outil.vue
@@ -9,56 +9,119 @@
     </header>
 
     <div class="menu-bar">
-      <p class='toggle' @click="toggleSidebar()" > > </p>
+      <p class='toggle' @click="toggleSidebar()"> > </p>
       <div class="menu">
         <ul class="menu-links">
-          <li class="nav-link">
-            <a href="#">
-              <span class="text nav-text">Dashboard</span>
+          <li class="nav-link" v-for="question in this.questions">
+            <a :href="question.id">
+              <span class="text nav-text">{{ question.name }}</span>
             </a>
-          </li>
-
-          <li class="nav-link">
-            <a href="#">
-              <span class="text nav-text">Revenue</span>
-            </a>
-          </li>
-
-          <li class="nav-link">
-            <a href="#">
-              <span class="text nav-text">Notifications</span>
-            </a>
-          </li>
-
-          <li class="nav-link">
-            <a href="#">
-              <span class="text nav-text">Analytics</span>
-            </a>
-          </li>
-
-          <li class="nav-link">
-            <a href="#">
-              <span class="text nav-text">Likes</span>
-            </a>
-          </li>
-
-          <li class="nav-link">
-            <span class="text nav-text">Wallets</span>
           </li>
 
         </ul>
       </div>
     </div>
-
   </nav>
+  <div class="content" :class="isSidebarClosed?'close-content':'show-content'">
+    <div class="questions">
+      <p style="margin-top: 25px;font-size: 45px"> {{ currentQuestion.question }}</p>
+      <div class="question-options">
+        <button v-for="options in currentQuestion.options">{{ options.name }}</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import {Question} from "~/interfaces/question";
+
 export default defineComponent({
-  name:"outil",
+  name: "outil",
   data: () => {
     return {
-      isSidebarClosed: true as boolean,
+      isSidebarClosed: false as boolean,
+      currentQuestion: {} as Question,
+      questions: [
+        {
+          id: 1,
+          name: "Questionnaire patient",
+          question: "Avez vous accès au questionnaire patient ?",
+          options: [
+            {
+              name: "oui",
+              value: 1,
+            },
+            {
+              name: "non",
+              value: 2,
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "Drapaux rouges",
+          question: "Vérifiez les drapeaux rouges",
+          options: [
+            {
+              name: "Un drapeau rouge présent",
+              value: 0
+            },
+            {
+              name: "Aucun drapeau rouge présent",
+              value: 3
+            }
+          ]
+        },
+        {
+          id: 3,
+          name: "Vertige maitenant ?",
+          question: "Le patient à-t-il des vertiges maitenant",
+          options: [
+            {
+              name: "oui",
+              value: 4
+            },
+            {
+              name: "non",
+              value: 5
+            },
+          ]
+        },
+        {
+          id: 4,
+          name: "HINTS",
+          question: "Vous aurez besoin :" + '<br>' +
+              "- Examen neurlogique" + '<br>' +
+              "- Examen ORL" + '<br>' +
+              "- DIX et Allpike" + '<br>' +
+              "- HINTS",
+          options: [
+            {
+              name: "suivant",
+              value: 6,
+            }
+          ]
+        },
+        {
+          id: 5,
+          name: "Nystagmus spontanné",
+          question: "Le patient souffre-t-il de nystagmus spontanné ?",
+          options: [
+            {
+              name: "Vertical",
+              value: 7,
+            },
+            {
+              name: "Horizonto-rotatoire",
+              value: 4,
+            },
+            {
+              name: "Non",
+              value: 5
+            }
+          ]
+        }
+      ] as Question[]
     };
   },
   methods: {
@@ -67,10 +130,31 @@ export default defineComponent({
       console.log(this.isSidebarClosed)
     },
   },
+  mounted() {
+    this.currentQuestion = this.questions.find((question: { id: number; }) => question.id == 1) as Question
+  }
 })
 </script>
 
 <style>
+.question-options {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.question-options button {
+  background-color: #31d8de;
+  border: none;
+  border-radius: 5px;
+  width: 300px;
+  padding: 7px;
+  margin: 1em;
+  font-size: 30px;
+  color: black;
+  box-shadow: 0 6px 18px -5px rgb(49, 216, 222);
+}
+
 * {
   margin: 0;
   padding: 0;
@@ -103,6 +187,7 @@ body {
   background-color: var(--primary-color);
   color: #fff;
 }
+
 /* ===== Sidebar ===== */
 .sidebar {
   position: fixed;
@@ -115,6 +200,7 @@ body {
   transition: var(--tran-05);
   z-index: 100;
 }
+
 .close {
   width: 0;
 }
@@ -156,9 +242,11 @@ body {
   white-space: nowrap;
   opacity: 1;
 }
+
 nav .close .text {
   opacity: 0;
 }
+
 /* =========================== */
 
 .sidebar header {
@@ -169,10 +257,12 @@ nav .close .text {
   display: flex;
   align-items: center;
 }
+
 .sidebar header .logo-text {
   display: flex;
   flex-direction: column;
 }
+
 header .image-text .name {
   margin-top: 2px;
   font-size: 18px;
@@ -191,11 +281,12 @@ header .image-text .profession {
   justify-content: center;
 }
 
-.close-image{
+.close-image {
   transform: translateX(-150%);
   transition: 0.3s;
 }
-.show-image{
+
+.show-image {
   transform: translateX(0);
   transition: 0.3s;
 }
@@ -206,7 +297,7 @@ header .image-text .profession {
   border-radius: 6px;
 }
 
-header .close{
+header .close {
   visibility: collapse;
 }
 
@@ -228,7 +319,7 @@ header .close{
   transition: var(--tran-05);
 }
 
-.sidebar .toggle::after{
+.sidebar .toggle::after {
   background-color: red;
   border-radius: 50%;
   content: "  ";
@@ -265,6 +356,7 @@ body.dark .sidebar header .toggle {
   font-weight: 500;
   transition: var(--tran-05);
 }
+
 .sidebar li a {
   list-style: none;
   height: 100%;
@@ -281,10 +373,12 @@ body.dark .sidebar header .toggle {
 .sidebar li a:hover {
   background-color: var(--primary-color);
 }
+
 .sidebar li a:hover .icon,
 .sidebar li a:hover .text {
   color: var(--sidebar-color);
 }
+
 body.dark .sidebar li a:hover .icon,
 body.dark .sidebar li a:hover .text {
   color: var(--text-color);
@@ -297,9 +391,11 @@ body.dark .sidebar li a:hover .text {
   justify-content: space-between;
   overflow-y: scroll;
 }
+
 .menu-bar::-webkit-scrollbar {
   display: none;
 }
+
 .sidebar .menu-bar .mode {
   border-radius: 6px;
   background-color: var(--primary-color-light);
@@ -315,15 +411,19 @@ body.dark .sidebar li a:hover .text {
 .mode .sun-moon i {
   position: absolute;
 }
+
 .mode .sun-moon i.sun {
   opacity: 0;
 }
+
 body.dark .mode .sun-moon i.sun {
   opacity: 1;
 }
+
 body.dark .mode .sun-moon i.moon {
   opacity: 0;
 }
+
 .home {
   position: absolute;
   top: 0;
@@ -334,6 +434,7 @@ body.dark .mode .sun-moon i.moon {
   background-color: var(--body-color);
   transition: var(--tran-05);
 }
+
 .home .text {
   font-size: 30px;
   font-weight: 500;
@@ -346,7 +447,35 @@ body.dark .mode .sun-moon i.moon {
   height: 100vh;
   width: calc(100% - 78px);
 }
+
 body.dark .home .text {
   color: var(--text-color);
+}
+
+.content {
+  width: auto;
+  margin-top: 80px;
+  margin-left: 50px;
+  margin-right: 50px;
+  box-shadow: 0 6px 18px -5px rgb(5, 5, 5);
+}
+
+.questions {
+  margin-right: 150px;
+  margin-top: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+
+.close-content {
+  transform: translateX(0);
+  transition: 0.3s;
+}
+
+.show-content {
+  transform: translateX(12%);
+  transition: 0.3s;
 }
 </style>
